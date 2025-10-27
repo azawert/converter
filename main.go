fbd6dd5469bcfb7c85c3d64e263412f4e3911d26
azawert <53710085+azawert@users.noreply.github.com>
2025-10-28 01:45:59 +0300

    Добавил функцию получения значений вводимых пользователем

--- main.go
+++ main.go
@@ -1,8 +1,20 @@
 package main
 
+import "fmt"
+
 func main() {
 	const UsdToEur = 0.86
 	const UsdToRub = 80
 
 	const EurToRub = UsdToRub / UsdToEur
+}
+
+func collectUserInputAndCount(amount int, currentCurrency string, wantedCurrency string) int {
+	fmt.Println("Collecting user input and count")
+	fmt.Println("Enter amount of money")
+	fmt.Scan(&amount)
+	fmt.Println("Enter current currency")
+	fmt.Scan(&currentCurrency)
+	fmt.Println("Enter wanted currency")
+	fmt.Scan(&wantedCurrency)
 }
